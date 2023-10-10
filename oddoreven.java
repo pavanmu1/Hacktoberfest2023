42ad8ca853b1424a100d98625158cb8a0d31f9ea
pavanmu1 <91820478+pavanmu1@users.noreply.github.com>
2023-10-10 11:08:28 +0530

    Create oddoreven.java

---
+++ oddoreven.java
@@ -0,0 +1,8 @@
+int number = 5;
+
+// Find out if the number above is even or odd
+if (number % 2 == 0) {
+  System.out.println(number + " is even.");
+} else {
+  System.out.println(number + " is odd.");
+}
